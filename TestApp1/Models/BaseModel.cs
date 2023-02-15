fb92485697bda029842294813acb4f19b0b763a3
user <user@DESKTOP-2KVT69E>
2023-02-15 14:13:19 +0800

    Add project files.

---
+++ TestApp1/Models/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApp1.Models
+{
+    public class BaseModel
+    {
+        public Guid GUID { get; set; }
+        public int ID { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ TestApp1/Models/Spell.cs
@@ -0,0 +1,46 @@
+﻿namespace TestApp1.Models
+{
+    public class Spell : BaseModel
+    {
+        public spell_type SpellType { get; set; }
+        public components Components { get; set; }
+        public classes Classes { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Range { get; set; }
+        public string Target { get; set; }
+        public float Duration { get; set; }
+        public string AtHigherLevels { get; set; }
+        public string MaterialComponents { get; set; }
+    }
+
+    public enum spell_type 
+    { 
+        abjuration,
+        alteration,
+        conjuration,
+        divination,
+        enchantment,
+        illusion,
+        invocation,
+        necromancy
+    }
+
+    public enum components
+    { 
+        V,
+        S,
+        M
+    }
+
+    public enum classes
+    { 
+        artificer,
+        bard,
+        cleric,
+        druid,
+        sorceror,
+        warlock,
+        wizard
+    }
+}
