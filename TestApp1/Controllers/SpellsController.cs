22c4297cb538ec7c38679579b615883f7e5fd978
Navin Gunna <nivanannug@gmail.com>
2023-03-07 14:20:47 +0800

    added SpellsController

---
+++ TestApp1/Controllers/SpellsController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TestApp1.Data;
+
+namespace TestApp1.Controllers
+{
+    [ServiceFilter(typeof(TestAsyncActionFilter))]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+
+    public class SpellsController : ControllerBase
+    {
+        private readonly ISpellRepo _repo;
+        private readonly IMapper _mapper;
+    }
+
+}
